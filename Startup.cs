44255b61b3d7adca42f3549582552e55d5b7dc6d
Denis Pessoa <dnspessoa1@gmail.com>
2021-02-01 11:33:43 -0300

    Add modelos

--- Startup.cs
+++ Startup.cs
@@ -44,11 +44,11 @@ namespace SmartSchool.WebAPI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SmartSchool.WebAPI v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
