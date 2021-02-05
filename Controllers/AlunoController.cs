c388b4897b500f3a0fde5a5b1c3deeb5a6cf8d38
Denis Pessoa <dnspessoa1@gmail.com>
2021-02-05 20:03:35 -0300

    Add get post put delete

--- Controllers/AlunoController.cs
+++ Controllers/AlunoController.cs
@@ -25,13 +25,83 @@ namespace SmartSchool.WebAPI.Controllers
             return Ok(Alunos);
         }
 
-        [HttpGet("{id}")]
+        //http://localhost:5000/api/aluno/8
+        // [HttpGet("{id:int}")]
+        // public IActionResult GetById(int id)
+        // {
+        //     // return Ok("Alunos: Marta, Paula, Lucas, Rafa"); //teste
+        //     var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+
+        //     if(aluno == null) 
+        //     {
+        //         return BadRequest("O Aluno não foi encontrado");
+        //     }
+        //     return Ok(aluno);
+        // }
+
+        // [HttpGet("{nome}")]
+        // public IActionResult GetByName(string nome)
+        // {
+        //     var aluno = Alunos.FirstOrDefault(a => a.Nome.Contains(nome));
+
+        //     if(nome == null)
+        //     return BadRequest("O Aluno não foi encontrado");
+
+        //     return Ok(aluno);
+        // }
+        
+        //http://localhost:5000/api/aluno/byId?id=1
+        [HttpGet("byId/{id}")]
         public IActionResult GetById(int id)
         {
             // return Ok("Alunos: Marta, Paula, Lucas, Rafa"); //teste
-            var alunos = Alunos.FirstOrDefault(a => a.Id == a.Id);
+            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
 
-            return Ok(alunos);
+            if(aluno == null) 
+            {
+                return BadRequest("O Aluno não foi encontrado");
+            }
+            return Ok(aluno);
         }
+
+        //http://localhost:5000/api/aluno/byName?nome=aNome&sobrenome=aSobrenome
+        [HttpGet("ByName")]
+        public IActionResult GetByName(string nome, string sobrenome)
+        {
+            var aluno = Alunos.FirstOrDefault(a => 
+                a.Nome.Contains(nome) && a.Sobrenome.Contains(sobrenome));
+
+            if(nome == null)
+            return BadRequest("O Aluno não foi encontrado");
+
+            return Ok(aluno);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Aluno aluno)
+        {
+
+            return Ok(aluno);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+
+        
     }
 }
